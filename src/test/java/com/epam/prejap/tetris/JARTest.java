9939bc1a9bf6adc055d4e10998b3df7f5434dcef
Andrii Velychko <velychko.andrii@gmail.com>
2021-06-06 19:37:34 +0200

    Change the way how JARTest class gets correct JAR file
    Hardcoded path to JAR file used in tests could cause troubles if project name or project version will change.
    This commit allows creation of .properties file which stores information from pom.xml, including correct JAR filename.
    This filename is later used in tests.

--- src/test/java/com/epam/prejap/tetris/JARTest.java
+++ src/test/java/com/epam/prejap/tetris/JARTest.java
@@ -3,32 +3,66 @@ package com.epam.prejap.tetris;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.jar.JarFile;
 
 import static org.testng.Assert.*;
 
 @Test
 public class JARTest {
-    private static final String SEPARATOR = File.separator;
-    private static final String JAR_PATH = "target" + SEPARATOR + "generated-test-sources" + SEPARATOR +
-            "test-jar" + SEPARATOR + "tetris-0.1-jar-test.jar";
     private static final String MAIN_CLASS_FQN = Tetris.class.getName();
+    private static final String PROPERTIES_FILE = "target" + File.separator + "generated-test-sources" +
+            File.separator + "test-jar" + File.separator + "properties-from-pom.properties";
+    private static final String JAR_PATH_PROPERTY = "test.jar.file.location";
 
-    public void jarFileForTestingIsCreated() {
-        File file = new File(JAR_PATH);
+    public void propertiesFileForJarTestsExists() {
+        File file = new File(PROPERTIES_FILE);
+        assertTrue(file.exists());
+    }
+
+    public void jarFileForTestingIsCreated() throws IOException {
+        PropertiesReader reader = new PropertiesReader(PROPERTIES_FILE);
+        String jarFilePath = reader.getProperty(JAR_PATH_PROPERTY);
+        File file = new File(jarFilePath);
         assertTrue(file.exists());
     }
 
     public void jarFileForTestingHasManifestFile() throws IOException {
-        JarFile file = new JarFile(JAR_PATH);
+        PropertiesReader reader = new PropertiesReader(PROPERTIES_FILE);
+        String jarFilePath = reader.getProperty(JAR_PATH_PROPERTY);
+        JarFile file = new JarFile(jarFilePath);
         assertNotNull(file.getManifest());
     }
 
     public void jarFileForTestingContainsCorrectMainClass() throws IOException {
-        JarFile file = new JarFile(JAR_PATH);
-        String mainClassFQN = file.getManifest().getMainAttributes().getValue("Main-Class");
+        PropertiesReader reader = new PropertiesReader(PROPERTIES_FILE);
+        String jarFilePath = reader.getProperty(JAR_PATH_PROPERTY);
+        JarFile file = new JarFile(jarFilePath);
+        String mainClassFQN = file.getManifest()
+                .getMainAttributes()
+                .getValue("Main-Class");
         assertEquals(mainClassFQN, MAIN_CLASS_FQN);
+    }
+
+    /**
+     * Helper class to get correct JAR file name.
+     * Eliminates the need to provide exact JAR filename every time the version or project name is changed.
+     */
+    private class PropertiesReader {
+        private final Properties properties;
+
+        PropertiesReader(String filePath) throws IOException {
+            InputStream is = new FileInputStream(filePath);
+            this.properties = new Properties();
+            this.properties.load(is);
+        }
+
+        String getProperty(String propertyName) {
+            return this.properties.getProperty(propertyName);
+        }
     }
 
 }
