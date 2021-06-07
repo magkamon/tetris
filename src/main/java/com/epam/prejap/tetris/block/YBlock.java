ee7606f5d533051decf48e66e2bac0f9bfc6947a
Anna Silenko <a.silenko@gmail.com>
2021-06-07 21:16:45 +0200

    correct javadoc and version

--- src/main/java/com/epam/prejap/tetris/block/YBlock.java
+++ src/main/java/com/epam/prejap/tetris/block/YBlock.java
@@ -1,4 +1,26 @@
+<<<<<<< HEAD
 package com.epam.prejap.tetris.block;/**
 @author Anna Silenko
 */public class YBlock {
+=======
+package com.epam.prejap.tetris.block;
+
+/**
+ *Represents Y block in BlockFeed
+ *
+ * @author Anna Silenko
+ * @see BlockFeed
+ */
+final class YBlock extends Block {
+
+    private static final byte[][] IMAGE = {
+            {1, 0, 1},
+            {0, 1, 0},
+            {0, 1, 0}
+    };
+
+    public YBlock() {
+        super(IMAGE);
+    }
+>>>>>>> f05a77c (correct javadoc and version)
 }
