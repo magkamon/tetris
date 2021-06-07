14f495a10b0378cdf6da15c1be35b495f735b70b
Kanybek Mukalaev <m.kanybek@mail.ru>
2021-06-07 23:18:40 +0200

    added java doc

--- src/main/java/com/epam/prejap/tetris/block/IBlock.java
+++ src/main/java/com/epam/prejap/tetris/block/IBlock.java
@@ -3,10 +3,13 @@ package com.epam.prejap.tetris.block;
 /**
  * @author Kanybek Mukalaev
  * @see BlockFeed
+ * @see Block
+ * @see com.epam.prejap.tetris.game.Printer
+ * It creates I block of tetris
  *
  */
 class IBlock extends Block {
-    public static final byte [][] image = {
+    static final byte [][] image = {
             {1},
             {1},
             {1},
