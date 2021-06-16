662ca16fe06c0439abd0bb659aac3635e5697eee
Anna Silenko <a.silenko@gmail.com>
2021-06-16 09:49:21 +0200

    Bump up project version and resolve conflicts

--- src/test/java/com/epam/prejap/tetris/block/OBlockTest.java
+++ src/test/java/com/epam/prejap/tetris/block/OBlockTest.java
@@ -9,7 +9,7 @@ import static org.testng.Assert.*;
  * @author Anna Silenko
  */
 
-@Test(groups = {"blockShapes", "blocks"})
+@Test(groups = "blockShapes")
 public class OBlockTest {
     OBlock oBlock = new OBlock();
 
