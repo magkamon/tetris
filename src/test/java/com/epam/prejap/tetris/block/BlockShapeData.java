6027c9f005055353e42121262185e3701f3e0497
cat-inGlasses <39728927+cat-inGlasses@users.noreply.github.com>
2021-06-18 03:33:51 +0200

    Add tests for SBlock
    Type of tests are generic,
    that means, we can use them for all shapes,
    since we are testing only parent class methods.
    Read class docs for usage.

---
+++ src/test/java/com/epam/prejap/tetris/block/BlockShapeData.java
@@ -0,0 +1,48 @@
+package com.epam.prejap.tetris.block;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Class holds data of shapes that should be for each block class
+ * <p>
+ * Creation date: 18/06/2021
+ *
+ * @author Nikita Pochapynskyi
+ */
+public class BlockShapeData {
+
+    /**
+     * Holds block object to be tested and and its expected image.
+     * Feel free to add more shapes here. They will be tested automatically.
+     */
+    final static Object[][] blocks = new Object[][]{
+            {new SBlock(), new byte[][]{
+                    {0, 1, 1},
+                    {1, 1, 0}
+            }}
+    };
+
+    /**
+     * Generates expected data for {@link Block#dotAt(int, int)} method
+     *
+     * @return {@link Iterator} with block object to be tested, coordinates of bytes and expected byte.
+     */
+    static Iterator<Object[]> getDotAtData() {
+
+        List<Object[]> data = new LinkedList<>();
+        for (Object[] block : blocks) {
+
+            byte[][] image = (byte[][]) block[1];
+
+            for (int i = 0; i < image.length; i++) {
+                for (int j = 0; j < image[i].length; j++) {
+                    data.add(new Object[]{block[0], i, j, image[i][j]});
+                }
+            }
+        }
+
+        return data.iterator();
+    }
+}
