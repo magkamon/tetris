afc95663d75e8f21e56ad97ab0cc62531fad02b8
cat-inGlasses <39728927+cat-inGlasses@users.noreply.github.com>
2021-06-20 14:11:27 +0200

    Add @Factory to test blocks. Created DP for it.

--- src/test/java/com/epam/prejap/tetris/block/BlockShapeData.java
+++ src/test/java/com/epam/prejap/tetris/block/BlockShapeData.java
@@ -1,8 +1,9 @@
 package com.epam.prejap.tetris.block;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import org.testng.annotations.DataProvider;
+
+import java.util.*;
+import java.util.function.Supplier;
 
 /**
  * Class holds data of shapes that should be for each block class
@@ -15,44 +16,65 @@ public class BlockShapeData {
 
     /**
      * Holds block object to be tested and and its expected image.
-     * Feel free to add more shapes here. They will be tested automatically.
+     * Feel free to add more shapes here (through static init block). They will be tested automatically.
      */
-    final static Object[][] blocks = new Object[][]{
-            {new SBlock(), new byte[][]{
-                    {0, 1, 1},
-                    {1, 1, 0}
-            }},
-            {new OBlock(), new byte[][]{
-                    {1, 1},
-                    {1, 1}
-            }},
-            {new TBlock(), new byte[][]{
-                    {1, 1, 1},
-                    {0, 1, 0}
-            }},
-            {new YBlock(), new byte[][]{
-                    {1, 0, 1},
-                    {0, 1, 0},
-                    {0, 1, 0}
-            }}
-    };
+    private static final Map<Supplier<Block>, byte[][]> blocks = new LinkedHashMap<>();
+
+    /**
+     * Holds mapping class to its ::new method reference
+     * This was done, because of creating object from Block.class reference is more complicated
+     */
+    private static final Map<Class<? extends Block>, Supplier<Block>> classToSupp = new HashMap<>();
+
+    // Add more shapes for testing here
+    static {
+        blocks.put(SBlock::new, new byte[][]{
+                {0, 1, 1},
+                {1, 1, 0}
+        });
+        blocks.put(OBlock::new, new byte[][]{
+                {1, 1},
+                {1, 1}
+        });
+        blocks.put(TBlock::new, new byte[][]{
+                {1, 1, 1},
+                {0, 1, 0}
+        });
+        blocks.put(YBlock::new, new byte[][]{
+                {1, 0, 1},
+                {0, 1, 0},
+                {0, 1, 0}
+        });
+    }
+
+    @DataProvider
+    public static Iterator<Object[]> blocks() {
+        List<Object[]> dpBlocks = new ArrayList<>(blocks.size());
+
+        blocks.forEach((Supplier<Block> blockNewRef, byte[][] expectedImage) -> {
+            Block blockToTest = blockNewRef.get();
+            dpBlocks.add(new Object[]{blockToTest, expectedImage});
+            classToSupp.put(blockToTest.getClass(), blockNewRef);
+        });
+
+        return dpBlocks.iterator();
+    }
+
 
     /**
      * Generates expected data for {@link Block#dotAt(int, int)} method
      *
      * @return {@link Iterator} with block object to be tested, coordinates of bytes and expected byte.
      */
-    static Iterator<Object[]> getDotAtData() {
+    static Iterator<Object[]> getDotAtDataFor(Class<? extends Block> clazz) {
 
         List<Object[]> data = new LinkedList<>();
-        for (Object[] block : blocks) {
 
-            byte[][] image = (byte[][]) block[1];
+        byte[][] image = blocks.get(classToSupp.get(clazz));
 
-            for (int i = 0; i < image.length; i++) {
-                for (int j = 0; j < image[i].length; j++) {
-                    data.add(new Object[]{block[0], i, j, image[i][j]});
-                }
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                data.add(new Object[]{i, j, image[i][j]});
             }
         }
 
